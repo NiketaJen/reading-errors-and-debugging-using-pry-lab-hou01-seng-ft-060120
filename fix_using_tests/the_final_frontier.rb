8cef46b75ab190d4defeb7e60dc6ab1d54b097ae
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-07 03:20:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,13 +5,13 @@ def generate_star_date
 end
 
 
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+def state_log($star_date)
+  "Captain's Log, star date #{$star_date}."
 end
 
 def engage
-  puts state_log(star_date)
-  date = generate_star_date
+  puts state_log($star_date)
+  $star_date = generate_star_date
 end
 
 binding.pry 
